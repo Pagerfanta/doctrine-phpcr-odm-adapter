1e10401c8bc044b57fae48105d8ba76d78459fb3
Michael Babker <michael.babker@gmail.com>
2023-10-11 22:12:36 -0400

    Add PHPCR ODM 2.x compat

---
+++ Tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use DG\BypassFinals;
+
+require __DIR__.'/../vendor/autoload.php';
+
+BypassFinals::setWhitelist([
+    '*/Doctrine/ODM/PHPCR/*',
+]);
+
+BypassFinals::enable(bypassReadOnly: false);
